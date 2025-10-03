dcbd313c84d0f88a33f4209d02772e6321b65726
YuriiYarkovich <yurii.yarkovich@innowise.com>
2025-10-03 12:53:37 +0000

    Moved database config to config class

--- apps/core-microservice/src/app.module.ts
+++ apps/core-microservice/src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
+import { DatabaseConfig } from './config/database.config';
 
 @Module({
   imports: [
@@ -15,29 +14,8 @@ import { join } from 'path';
       ],
     }),
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        /*console.log('DB CONFIG:', {
-          host: config.get<string>('POSTGRES_HOST'),
-          port: config.get<number>('POSTGRES_PORT'),
-          user: config.get<string>('POSTGRES_USER'),
-          pass: config.get<string>('POSTGRES_PASSWORD'),
-        });*/
-        return {
-          type: 'postgres',
-          host: config.get<string>('POSTGRES_HOST'),
-          port: config.get<number>('POSTGRES_PORT'),
-          username: config.get<string>('POSTGRES_USER'),
-          password: config.get<string>('POSTGRES_PASSWORD'),
-          database: config.get<string>('POSTGRES_DB'),
-          autoLoadEntities: true,
-          synchronize: true,
-        };
-      },
+      useClass: DatabaseConfig,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
---
+++ apps/core-microservice/src/config/database.config.ts
@@ -0,0 +1,21 @@
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class DatabaseConfig implements TypeOrmOptionsFactory {
+  constructor(private readonly config: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.config.get<string>('POSTGRES_HOST'),
+      port: this.config.get<number>('POSTGRES_PORT'),
+      username: this.config.get<string>('POSTGRES_USER'),
+      password: this.config.get<string>('POSTGRES_PASSWORD'),
+      database: this.config.get<string>('POSTGRES_DB'),
+      autoLoadEntities: true,
+      synchronize: true,
+    };
+  }
+}
