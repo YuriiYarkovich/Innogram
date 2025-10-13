ef6d4f44e7e2ae4a31bae352475a3372f2516833
YuriiYarkovich <yurii.yarkovich@innowise.com>
2025-10-13 16:36:40 +0000

    Set up redis connection

--- apps/auth_microservice/src/main.ts
+++ apps/auth_microservice/src/main.ts
@@ -1,4 +1,18 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from 'express';
+import { createClient } from 'redis';
+
+const redisClient = createClient();
+(async () => {
+  redisClient.on('error', (err) => {
+    console.log(`Redis client error: `, err);
+  });
+
+  redisClient.on('ready', () => {
+    console.log(`Redis client started!`);
+  });
+  await redisClient.connect();
+  await redisClient.ping();
+})();
 
 const app = express();
 
