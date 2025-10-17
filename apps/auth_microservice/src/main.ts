f14dca082f6f67ccf5b8942b4483bb1dcb617a09
YuriiYarkovich <yurii.yarkovich@innowise.com>
2025-10-17 15:11:55 +0000

    Added cors and security middleware

--- apps/auth_microservice/src/main.ts
+++ apps/auth_microservice/src/main.ts
@@ -5,11 +5,19 @@ import passport from 'passport';
 import { errorHandlingMiddleware } from './middleware/error-handling.middleware.ts';
 import './config/load-env.config.ts';
 import { initRedis } from './config/redis.init.ts';
+import helmet from 'helmet';
+import cors from 'cors';
 
 const PORT = process.env.AUTH_SERVICE_PORT;
 
 const app = express();
 app.use(express.json());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginEmbedderPolicy: false,
+  }),
+);
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -19,6 +27,14 @@ app.use(
       secure: false, //TODO change to secure after switching to httpS
       maxAge: 24 * 60 * 60 * 1000,
     },
+  }),
+);
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:3000', // разрешённый источник
+    credentials: true, // если используешь cookies или сессии
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   }),
 );
 app.use(passport.initialize());
